0fa54f59c08b8e254ed65b9e6eea755c70feffb0
Victor Queiroz <victorqueiroz.ti@gmail.com>
2025-08-24 05:10:07 -0300

    feat: delivery-tracking webclient

---
+++ Microsservices/Delivery-Tracking/src/main/java/com/qrzsolutions/qrzdelivery/delivery/tracking/infrastructure/http/client/CourierPayoutCalculationServiceHttpImpl.java
@@ -0,0 +1,24 @@
+package com.qrzsolutions.qrzdelivery.delivery.tracking.infrastructure.http.client;
+
+import com.qrzsolutions.qrzdelivery.delivery.tracking.domain.service.CourierPayoutCalculationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CourierPayoutCalculationServiceHttpImpl
+        implements CourierPayoutCalculationService {
+
+    private final CourierAPIClient courierAPIClient;
+
+    @Override
+    public BigDecimal calculatePayout(Double distanceInKm) {
+        var courierPayoutResultModel = courierAPIClient.payoutCalculation(
+                new CourierPayoutCalculationInput(distanceInKm)
+        );
+        return courierPayoutResultModel.getPayoutFee();
+    }
+
+}
